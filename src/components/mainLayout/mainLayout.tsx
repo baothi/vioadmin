32c50fdc56f6880d484e2aef10d67e7ac16d7df6
khangtranpt <khang.tran@prismtechinc.io>
2023-12-12 00:09:11 +0700

    fix trigger on mobile

--- src/components/mainLayout/mainLayout.tsx
+++ src/components/mainLayout/mainLayout.tsx
@@ -2,34 +2,23 @@ import type { MenuProps } from 'antd';
 import { Layout, theme } from 'antd';
 import SiderX from '../sider/menuSider';
 import { Outlet } from "react-router-dom";
-import { useMediaQuery } from 'react-responsive';
+import './mainLayout.scss'
 
-const { Header, Content, Footer } = Layout;
-
-const items1: MenuProps['items'] = ['1', '2', '3'].map((key) => ({
-  key,
-  label: `nav ${key}`,
-}));
-
+const {Content, Footer } = Layout;
 
 const MainLayout: React.FC = () => {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
-  const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
+
   return (
     <Layout>
-      <Content style={{ padding: '0px' }}>
-        <Layout style={{ padding: '10px 0', background: colorBgContainer,  }}>
+      <Content className='main-content' >
+        <Layout className='main-layout' style={{background: colorBgContainer}}>
          <SiderX />
-          <Content style={{
-            overflow: isMobile ? "hidden" :'cover',
-            padding: isMobile ?   '0' : '0 24px',
-            minHeight: 500,
-            margin: isMobile ? '0' : "0px 16px",
-            background: colorBgContainer, 
-            maxWidth: isMobile ? "inherits" : "inherits" 
-            }}><Outlet /></Content>
+          <Content className='sub-content' style={{background: colorBgContainer, }}
+            ><Outlet />
+          </Content>
         </Layout>
       </Content>
       <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
